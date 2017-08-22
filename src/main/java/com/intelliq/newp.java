bafeebfa7d5b515a17414188be975742ddb7c77f
Sai Krishna <selenium.saikrishna@gmail.com>
2017-08-22 02:33:41 -0700

    First commit

---
+++ src/main/java/com/intelliq/newp.java
@@ -0,0 +1,14 @@
+package com.intelliq;
+
+
+public class newp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("abc");
+	}
+
+}
---
+++ src/test/java/com/intelliq/BrowserLaunch.java
@@ -0,0 +1,12 @@
+package com.intelliq;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserLaunch {
+	
+	public static void main(String[] args)
+	{
+		FirefoxDriver driver=new FirefoxDriver();
+	}
+
+}
